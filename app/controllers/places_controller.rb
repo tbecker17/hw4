0aed5b456d6743616f78d6244a4a37ddad936eaa
Tim Becker <tim.becker@kellogg.northwestern.edu>
2023-05-30 22:21:50 +0000

    Inserted CSS and navbar

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,23 +1,25 @@
 class PlacesController < ApplicationController
-
   def index
     @places = Place.all
   end
 
-  def show
-    @place = Place.find_by({ "id" => params["id"] })
-    @posts = Post.where({ "place_id" => @place["id"] })
-  end
-
   def new
-    @place = Place.new
+    # Render new place form
   end
 
   def create
-    @place = Place.new
-    @place["name"] = params["place"]["name"]
-    @place.save
-    redirect_to "/places"
+    place = Place.new(place_params)
+    place.user = @current_user
+    if place.save
+      redirect_to places_path
+    else
+      render :new
+    end
   end
 
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :description)
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to places_path
+    else
+      redirect_to login_path, alert: "Invalid email or password"
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to login_path
   end
 end
   
