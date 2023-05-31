5bf0a628d705726cc8753391690eb376bc39092f
Tim Becker <tim.becker@kellogg.northwestern.edu>
2023-05-31 16:15:10 +0000

    Updated application controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,27 @@
 class SessionsController < ApplicationController
   def new
   end
-
+  
   def create
-    user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect_to places_path
+    @user = User.find_by(email: params[:email])
+    if @user
+      if BCrypt::Password.new(@user.password) == params[:password]
+        session[:user_id] = @user.id
+        flash[:notice] = "Hello."
+        redirect_to posts_path
+      else
+        flash[:notice] = "Nope."
+        redirect_to login_path
+      end
     else
-      redirect_to login_path, alert: "Invalid email or password"
+      flash[:notice] = "Nope."
+      redirect_to login_path
     end
   end
 
   def destroy
     session[:user_id] = nil
+    flash[:notice] = "Goodbye."
     redirect_to login_path
   end
 end
-  
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,11 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new
-    @user["username"] = params["user"]["username"]
+    @user["first_name"] = params["user"]["first_name"]
+    @user["last_name"] = params["user"]["last_name"]
     @user["email"] = params["user"]["email"]
-    @user["password"] = params["user"]["password"]
+    @user["password"] = BCrypt::Password.create(params["user"]["password"])
     @user.save
-    redirect_to "/"
+    redirect_to "/posts"
   end
 end
