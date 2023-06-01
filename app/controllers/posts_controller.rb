f221fb622f9611c41b9b9b37569b8573c859a695
Tim Becker <tim.becker@kellogg.northwestern.edu>
2023-06-01 00:48:33 +0000

    Attempting to fix post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,4 +20,11 @@ class PostsController < ApplicationController
     end
     redirect_to "/posts"
   end
+  before_action :allow_cors
+   def allow_cors
+     response.headers['Access-Control-Allow-Origin'] = '*'
+     response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+     response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-User-Token, X-User-Email'
+     response.headers['Access-Control-Max-Age'] = '1728000'
+   end
 end
